6252f4c51b64f4d7bbc7d8311882edb23317569c
Your Name <you@example.com>
2024-08-05 15:07:56 -0500

    Creating CSRF and CORS configurations

--- src/main/java/com/course/springdata/web/config/SecurityConfig.java
+++ src/main/java/com/course/springdata/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,9 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth ->
                 auth.requestMatchers("/**")
                         .authenticated()
-        ).httpBasic(Customizer.withDefaults());
+        ).csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
