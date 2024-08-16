130bb982fb6b6548322defd1d1eaa950fda8242b
Your Name <you@example.com>
2024-08-15 21:56:07 -0500

    Creating users table

---
+++ src/main/java/com/course/springdata/persistence/entity/User.java
@@ -0,0 +1,32 @@
+package com.course.springdata.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "smallint")
+    private Boolean locked;
+
+    @Column(nullable = false, columnDefinition = "smallint")
+    private Boolean disabled;
+}
