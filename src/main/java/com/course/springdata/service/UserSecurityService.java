05e8beeeefa86774fb1e47dc85598a0737cdee89
Your Name <you@example.com>
2024-09-25 22:08:00 -0500

    Using authorities for access

--- src/main/java/com/course/springdata/service/UserSecurityService.java
+++ src/main/java/com/course/springdata/service/UserSecurityService.java
@@ -1,13 +1,19 @@
 package com.course.springdata.service;
 
 import com.course.springdata.persistence.entity.UserEntity;
+import com.course.springdata.persistence.entity.UserRole;
 import com.course.springdata.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserSecurityService implements UserDetailsService {
@@ -23,13 +29,37 @@ public class UserSecurityService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity user = this.userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User "+username+" not found"));
+
+        String[] roles = user.getRoles().stream().map(UserRole::getRole).toArray(String[]::new);
         return User
                 .builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
-                .roles("ADMIN")
+                .authorities(this.grantedAuthorities(roles))
                 .accountLocked(user.getLocked())
                 .disabled(user.getDisabled())
                 .build();
+    }
+
+    private String[] getAuthorities(String role){
+        if("ADMIN".equals(role) || "CUSTOMER".equals(role)){
+            return new String[] {"random_order"};
+        }
+
+        return new String[]{};
+    }
+
+    private List<GrantedAuthority> grantedAuthorities (String[] roles){
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+
+        for(String role: roles){
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+
+            for(String authority: this.getAuthorities(role)){
+                authorities.add((new SimpleGrantedAuthority(authority)));
+            }
+        }
+
+        return authorities;
     }
 }
